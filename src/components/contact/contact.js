29ec749c112e23d47dff304787355baa23f0b8c4
Tran Ong <baotranongtran@gmail.com>
2023-01-04 14:53:32 -0500

    updates-6

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,9 +1,14 @@
 import ContactCSS from './contact.module.css';
+import { Button } from '@mui/material';
 
 const Section4 = () => {
     return (
         <div className={ContactCSS.container}>
-
+            <div className={ContactCSS.text}>
+            <p style = {{fontSize: 'clamp(40px, 8vw, 80px)', 
+                fontFamily: '"Calibre","Inter","San Francisco","SF Pro Text",-apple-system,system-ui,sans-serif,"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace', 
+                fontWeight: 'bolder', padding: '4vh 0px 4px 0px'}}> Get in touch. </p>
+            </div>
         </div>
     )
 }
