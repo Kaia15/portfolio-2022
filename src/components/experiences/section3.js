288d08e07dbd822f8494838ab2448af534ce2565
Tran Ong <baotranongtran@gmail.com>
2023-01-03 23:54:18 -0500

    updates-5

--- src/components/experiences/section3.js
+++ src/components/experiences/section3.js
@@ -9,6 +9,9 @@ const Section3 = () => {
     const links = ['http://boost.associates/', 'https://denison.edu/']
     const descriptions = ['', 'Instructed 30+ high school students to join Florence Day in Ohio, to promote career opportunities in statistics and data science.']
     const a = [0,1]
+    const hl = ['Skills: ', 'Awards: ']
+    const d = ['React.js, Express.js, APIs, Material UI, HTML, CSS', '']
+
     return (
         <div className = {ExpCSS.container}>
             <div className={ExpCSS.d1}>
@@ -31,9 +34,9 @@ const Section3 = () => {
                     {nums.map((num,id) => {
                         return (
                             <Card style = {{margin: '12px 12vw', minWidth: '275px', backgroundColor: '#0a192f', color: '#ccd6f6', textAlign: 'left'}}>
-                        <CardContent>
+                            <CardContent>
                             <Typography sx={{ fontSize: 14, color: '#ccd6f6' }} color="text.secondary" gutterBottom>
-                            <i><a href = {links[id]}> {coms[id]} </a></i>
+                            <i><a href = {links[id]} style = {{textDecoration: 'none'}}> {coms[id]} </a></i>
                             </Typography>
                             <Typography variant="h5" component="div" style = {{color: '#64ffda'}}>
                             {num}
@@ -62,7 +65,24 @@ const Section3 = () => {
                 <div style = {{flex: '4'}}></div>
                 <div style = {{flex: '4'}}></div>
                 </div>
-                <div className={ExpCSS.d12}></div>
+                <div className={ExpCSS.d12}>
+                    <Card style = {{margin: '12px 12vw', minWidth: '275px', backgroundColor: '#0a192f', color: '#ccd6f6', textAlign: 'left'}}>
+                    <CardContent>
+                        {hl.map((item,id) => {
+                            return (
+                                <div style = {{display: 'flex', flexDirection: 'row'}}>
+                                <Typography sx={{ fontSize: 18, color: '#64ffda', margin: '0px 8px'}} color="text.secondary" gutterBottom>
+                                <i> {item} </i>
+                                </Typography>
+                                <Typography sx={{ mb: 1.5, color: '#ccd6f6' }} color="text.secondary">
+                                    {d[id]}
+                                </Typography>
+                                </div>
+                            )
+                        })}
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </div>
     )
