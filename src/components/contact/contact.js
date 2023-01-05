a1485481d3d032a940a6288d5117fb65ce1b1a7e
Tran Ong <baotranongtran@gmail.com>
2023-01-04 20:00:00 -0500

    04.01.23-7:59

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -11,7 +11,7 @@ const Section4 = () => {
             <div className={ContactCSS.text}>
             <p style = {{fontSize: 'clamp(40px, 8vw, 80px)', 
                 fontFamily: '"Calibre","Inter","San Francisco","SF Pro Text",-apple-system,system-ui,sans-serif,"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace', 
-                fontWeight: 'bolder', margin: '64px 0px 0px 0px', color: 'rgb(100, 255, 218)', textAlign: 'center'}}> Get in touch </p>
+                fontWeight: 'bolder', margin: '64px 0px 0px 0px', color: 'rgb(100, 255, 218)', textAlign: 'center'}}> Get in touch. </p>
             <p style={{color: '#a8b2d1', margin: '0px 12px', fontSize: '18px'}}> I'm currently based in the U.S. <br /> 
             I am looking forward to any tech opportunities <br /> with any locations inside and outside the U.S.
             </p>
