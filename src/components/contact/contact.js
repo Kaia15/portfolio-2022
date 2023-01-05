f47a08580dfd07902551eac1fd8e8ae8fab680d5
Tran Ong <baotranongtran@gmail.com>
2023-01-04 19:47:32 -0500

    updates-7

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,9 @@
 import ContactCSS from './contact.module.css';
 import { Button } from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MailIcon from '@mui/icons-material/Mail';
 
 const Section4 = () => {
     return (
@@ -7,7 +11,16 @@ const Section4 = () => {
             <div className={ContactCSS.text}>
             <p style = {{fontSize: 'clamp(40px, 8vw, 80px)', 
                 fontFamily: '"Calibre","Inter","San Francisco","SF Pro Text",-apple-system,system-ui,sans-serif,"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace', 
-                fontWeight: 'bolder', padding: '4vh 0px 4px 0px'}}> Get in touch. </p>
+                fontWeight: 'bolder', margin: '64px 0px 0px 0px', color: 'rgb(100, 255, 218)', textAlign: 'center'}}> Get in touch </p>
+            <p style={{color: '#a8b2d1', margin: '0px 12px', fontSize: '18px'}}> I'm currently based in the U.S. <br /> 
+            I am looking forward to any tech opportunities <br /> with any locations inside and outside the U.S.
+            </p>
+            <div className = {ContactCSS.icons}>
+                <InstagramIcon fontSize='large'/>
+                <LinkedInIcon fontSize='large'/>
+                <GitHubIcon fontSize='large'/>
+                <MailIcon fontSize='large'/>
+            </div>
             </div>
         </div>
     )
